5abea291e13ed5810e70e916e77f9fc7c4004984
gmc-code <gmccarthy@tpg.com.au>
2024-06-04 15:05:47 +1000

    tk

--- docs/tkinter_basics/files/geometry/tk_place.py
+++ docs/tkinter_basics/files/geometry/tk_place.py
@@ -5,13 +5,15 @@ window = tk.Tk()
 window.title('Layout intro')
 window.geometry('600x400')
 
-# widgets 
+# widgets
 label1 = tk.Label(window, text = 'Label 1', background = 'red')
-label2 = tk.Label(window, text = 'Label 2', background = 'green')
+label2 = tk.Label(window, text = 'Label 2', background = 'pink')
+label3 = tk.Label(window, text = 'Label 3', background = 'orange')
 
 # place
-label1.place(x = 100 , y = 100, width = 200, height = 100)
-label2.place(x = 200 , y = 200, width = 200, height = 100)
+label1.place(x=50, y=40)
+label2.place(x=50, y=75, width=60, height=80)
+label3.place(x=110, y=155, anchor="center")
 
 # run
 window.mainloop()
