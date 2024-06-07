ddbe0bf5f8d618f53396d899aa3385cb746b6196
gmc-code <gmccarthy@tpg.com.au>
2024-06-07 14:30:22 +1000

    tk

---
+++ docs/tkinter_project/files/dict_hex_from_color1.py
@@ -0,0 +1,25 @@
+# Dictionary with rainbow colors
+rainbow_colors = {
+    "red": "#FF0000",
+    "orange": "#FFA500",
+    "yellow": "#FFFF00",
+    "green": "#008000",
+    "blue": "#0000FF",
+    "indigo": "#4B0082",
+    "violet": "#EE82EE"
+}
+
+user_color = input('Enter a rainbow color (red, orange, yellow, green, blue, indigo, violet): ')
+
+def return_hex_color(user_color):
+    # Convert the input to lowercase for case-insensitivity
+    user_color = user_color.lower()
+
+    # Check if the input color exists in the rainbow_colors dictionary
+    if user_color in rainbow_colors:
+        return rainbow_colors[user_color]
+    else:
+        return "not a valid colour of the rainbow"
+
+hex_val = return_hex_color(user_color)
+print(f"The hexadecimal value for {user_color} is {hex_val}")
